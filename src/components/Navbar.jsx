0bd060df5b237ad9e5313e4b05625bda20a66350
Shreyash Gupta <79988299+ghostshreyash@users.noreply.github.com>
2024-09-14 12:44:51 +0530

    updated the logo fille path

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = ({ user, onLogout }) => {
   return (
     <nav className="container" style={{ background: 'none', padding: '10px 20px', display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
       <div style={{ display: 'flex', alignItems: 'center' }}>
-        <img src="./src/assets/logo.png" alt="Recipe Wizard Logo" />
+        <img src="./assets/logo.png" alt="Recipe Wizard Logo" />
         <div style={{ marginLeft: '20px', display: 'flex' }}>
           <Link to="/" style={{ margin: '0 100px', textDecoration: 'none', color: 'black', fontWeight: 'bold', fontSize:'30px' }}>Home</Link>
           <Link to="/recipes" style={{ textDecoration: 'none', color: 'black', fontWeight: 'bold', fontSize: '30px' }}>Recipes</Link>
